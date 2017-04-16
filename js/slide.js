f714b7f472aea22160d76a3112018956f0883527
Jerrywang <luckboystudent@gmail.com>
2017-04-16 21:42:00 +0800

    first
    version1

---
+++ js/slide.js
@@ -0,0 +1,134 @@
+
+	$(document).ready(function(){
+		// var child=$('this').each
+		$('.new,.news').hover(
+		  function() {
+		    $( '.news' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    	// position:''
+		    	// 'border':'solid 1px black'
+		    });
+
+		  }, function() {
+		   	$( '.news' ).css({
+		    	display:'none'
+		    });
+		  }
+		);
+
+		$('.clooth,.cloothes').hover(
+		  function() {
+		    $( '.cloothes' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px'
+		    });
+
+		  }, function() {
+		    $( '.cloothes' ).css({
+		    	display:'none',
+
+		    });
+
+		  }
+		);
+		$('.shoe,.shoes').hover(
+		  function() {
+		    $( '.shoes' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+
+		  }, function() {
+		    $( '.shoes' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+		$('.sweater,.sweateres').hover(
+		  function() {
+		    $( '.sweateres' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+		  }, function() {
+		    $( '.sweateres' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+		$('.Accessory,.Accessories').hover(
+		  function() {
+		    $( '.Accessories' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+		  }, function() {
+		    $( '.Accessories' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+		$('.backpack,.backpacks').hover(
+		  function() {
+		    $( '.backpacks' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+		  }, function() {
+		    $( '.backpacks' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+		$('.sport,.sports').hover(
+		  function() {
+		    $( '.sports' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+		  }, function() {
+		    $( '.sports' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+		$('.men,.mens').hover(
+		  function() {
+		    $( '.mens' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+		  }, function() {
+		    $( '.mens' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+		$('.brand,.brands').hover(
+		  function() {
+		    $( '.brands' ).css({
+		    	display:'block',
+		    	'margin':'0px 0px 300px 0px',
+		    });
+
+		  }, function() {
+		    $( '.brands' ).css({
+		    	display:'none'
+		    });
+
+		  }
+		);
+	});
